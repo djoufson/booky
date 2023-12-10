ae04cd9d8e287b48bef5c22b0fefa0321b6a618e
Djoufson CHE BENE <djouflegran@gmail.com>
2023-12-10 22:17:56 +0100

    refactor: Enhance read performances using compiled query

--- src/Services/Catalog.API/Endpoints/GetAllBooks.cs
+++ src/Services/Catalog.API/Endpoints/GetAllBooks.cs
@@ -1,5 +1,6 @@
 using Catalog.API.Contracts.Dtos;
 using Catalog.API.Infra.Data;
+using Catalog.API.Models;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,9 +8,14 @@ namespace Catalog.API.Endpoints;
 
 public partial class CatalogEndpoints
 {
+    // https://learn.microsoft.com/ef/core/performance/advanced-performance-topics#compiled-queries
+    private static readonly Func<CatalogDbContext, IAsyncEnumerable<Book>> s_getCatalogQuery =
+        EF.CompileAsyncQuery((CatalogDbContext ctx) => ctx.Books.AsNoTracking());
+
     public static async Task<Results<Ok<BookDto[]>, BadRequest>> GetAllBooks(CatalogDbContext context)
     {
-        var books = await context.Books.ToArrayAsync();
+        var books = await ToListAsync(s_getCatalogQuery(context));
+
         var response = books.Select(b => new BookDto(
             b.Id.Value.ToString(),
             b.Title,
@@ -29,5 +35,16 @@ public partial class CatalogEndpoints
             b.UpdatedAt
         )).ToArray();
         return TypedResults.Ok(response);
+    }
+
+    private static async Task<List<T>> ToListAsync<T>(IAsyncEnumerable<T> asyncEnumerable)
+    {
+        var results = new List<T>();
+        await foreach (var item in asyncEnumerable)
+        {
+            results.Add(item);
+        }
+
+        return results;
     }
 }
