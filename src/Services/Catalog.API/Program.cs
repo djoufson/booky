5945621a93f8c81c2320a9970b79246ed0f7adfa
Djoufson CHE BENE <djouflegran@gmail.com>
2023-12-10 13:22:59 +0100

    setup: Add Docker support to the Catalog.API service

--- src/Services/Catalog.API/Program.cs
+++ src/Services/Catalog.API/Program.cs
@@ -16,7 +16,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.MapGroup("/api/catalog")
     .WithTags("Catalog API")
