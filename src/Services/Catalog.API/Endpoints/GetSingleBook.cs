9512b04c32936c44356cda20bd139c653ed8dd52
Djoufson CHE BENE <djouflegran@gmail.com>
2023-12-20 22:58:35 +0100

    feat: Implement feature to retrieve only a single book

---
+++ src/Services/Catalog.API/Endpoints/GetSingleBook.cs
@@ -0,0 +1,42 @@
+using Catalog.API.Contracts.Dtos;
+using Catalog.API.Infra.Data;
+using Catalog.API.Models.Types;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.API.Endpoints;
+
+public partial class CatalogEndpoints
+{
+    public static async Task<Results<Ok<BookDto>, NotFound>> GetSingleBook(Guid id, CatalogDbContext context)
+    {
+        var bookId = new BookId(id);
+        var book = await context.Books
+            .Include(b => b.Author)
+            .FirstOrDefaultAsync(b => b.Id == bookId);
+
+        if(book is null)
+            return TypedResults.NotFound();
+
+        var bookDto = new BookDto(
+            book.Id.Value.ToString(),
+            book.Title,
+            book.Description,
+            book.Price,
+            book.OldPrice,
+            new AuthorDto(
+                book.AuthorId.Value.ToString(),
+                book.Author.UserName.Value,
+                book.Author.Name.First,
+                book.Author.Name.Last,
+                book.Author.Email.Value,
+                book.Author.Bio,
+                book.Author.ImageUrl
+            ),
+            book.CreatedAt,
+            book.UpdatedAt,
+            book.Tags.Select(t => t.Tag).ToArray()
+        );
+        return TypedResults.Ok(bookDto);
+    }
+}
