15907de55502cbb7967c56f0cf9bfc2314ea53a3
Djoufson CHE BENE <djouflegran@gmail.com>
2023-12-23 15:18:28 +0100

    setup: Create the frontend application, and setup the load balancer / api gateway project

--- src/Aspire/Aspire.AppHost/Program.cs
+++ src/Aspire/Aspire.AppHost/Program.cs
@@ -5,14 +5,23 @@ var rabbitMq = builder.AddRabbitMQContainer("rabbitmq");
 var catalogDb = postgres.AddDatabase("CatalogDb");
 var identityDb = postgres.AddDatabase("IdentityDb");
 
-builder
+var catalogApi = builder
     .AddProject<Projects.Catalog_API>("catalog-api")
     .WithReference(catalogDb)
     .WithReference(rabbitMq);
 
-builder
+var identityApi = builder
     .AddProject<Projects.Identity_API>("identity-api")
     .WithReference(identityDb)
     .WithReference(rabbitMq);
+
+var yarpLoadBalancer = builder
+    .AddProject<Projects.LoadBalancer>("api")
+    .WithReference(identityApi)
+    .WithReference(catalogApi);
+
+builder
+    .AddProject<Projects.Web>("web")
+    .WithReference(yarpLoadBalancer);
 
 builder.Build().Run();
