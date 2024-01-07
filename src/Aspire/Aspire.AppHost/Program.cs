4ef5ee1826421ed62f1189bc650857d0a575b5bf
Djoufson CHE BENE <djouflegran@gmail.com>
2024-01-07 13:23:55 +0100

    setup: Create the Basket Service

--- src/Aspire/Aspire.AppHost/Program.cs
+++ src/Aspire/Aspire.AppHost/Program.cs
@@ -3,6 +3,7 @@ var postgres = builder.AddPostgresContainer("postgres");
 
 var catalogDb = postgres.AddDatabase("CatalogDb");
 var identityDb = postgres.AddDatabase("IdentityDb");
+var basketDb = postgres.AddDatabase("BasketDb");
 
 var catalogApi = builder
     .AddProject<Projects.Catalog_API>("catalog-api")
@@ -12,10 +13,15 @@ var identityApi = builder
     .AddProject<Projects.Identity_API>("identity-api")
     .WithReference(identityDb);
 
+var basketApi = builder
+    .AddProject<Projects.Basket_API>("basket-api")
+    .WithReference(basketDb);
+
 var api = builder
     .AddProject<Projects.LoadBalancer>("api")
     .WithReference(identityApi)
-    .WithReference(catalogApi);
+    .WithReference(catalogApi)
+    .WithReference(basketApi);
 
 builder
     .AddProject<Projects.Web>("web")
