27dc7deb793c80f25a01334622bbfb9550d0b08b
Djoufson CHE BENE <djouflegran@gmail.com>
2024-01-14 10:56:40 +0100

    ci: Add the build-publish workflow

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,4 +1,4 @@
-# !/bin/bash
+#!/bin/bash
 # Script that applies the k8s configurations
 
 # Redis cache
---
+++ k8s/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Setup (build-publish) the necessary Docker images to deploy our infrastructure on k8s
+
+# Load DockerHub username from .env file
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+fi
+
+# Build Docker images
+docker-compose -f docker-compose.build.yml config --services | xargs -I {} docker-compose -f docker-compose.build.yml build {}
+
+# Set DockerHub username from .env file
+DOCKER_USERNAME=${DOCKER_USERNAME:-your_default_username}
+
+IMAGE_NAME_PREFIX=$DOCKER_USERNAME/your_repository_name # Replace with your DockerHub repository name
+
+# Login to DockerHub
+docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
+
+# Loop through the services in docker-compose.build.yml
+for service in $(docker-compose -f docker-compose.build.yml config --services); do
+    # Tag Docker images
+    docker tag $service:latest $IMAGE_NAME_PREFIX/$service:latest
+    echo "Docker image tagged as $IMAGE_NAME_PREFIX/$service:latest"
+
+    # Push Docker images to DockerHub
+    docker push $IMAGE_NAME_PREFIX/$service:latest
+    echo "Docker image pushed to DockerHub: $IMAGE_NAME_PREFIX/$service:latest"
+done
